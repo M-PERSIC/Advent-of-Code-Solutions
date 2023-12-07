04566c6479cab6f604a4b5056db7ac97a7deeba4
Michael Persico <michael.a.persico@gmail.com>
2023-12-07 20:28:37 +0000

    Add Day 07 template

--- src/Day 05/day05.jl
+++ src/Day 05/day05.jl
@@ -32,9 +32,35 @@ function part_one(input::String)
     return minimum(seeds)
 end
 
-println(part_one("input_part_one.txt"))
+# println(part_one("input_part_one.txt"))
 
 # TODO
-function part_two(input::String) end
+function part_two(input::String) 
+    maps = split(readchomp(input), "\n\n")
+    seeds = parse.(Int, split(maps[1])[2:end])
+    seeds = vcat([collect(seeds[x]:(seeds[x] + seeds[x + 1])) for x in 1:2:length(seeds) - 1]...)
+    for map in maps[2:end]
+        source_ranges, dest_range_starts = Vector{UnitRange}(), Vector{Int}()
+        seed_ranges = split(map, "\n")[2:end]
+        for seed_range in seed_ranges
+            seed_range_nums = parse.(Int, String.(split(seed_range)))
+            push!(
+                source_ranges,
+                UnitRange(seed_range_nums[2], seed_range_nums[2] + seed_range_nums[3] - 1),
+            )
+            push!(dest_range_starts, seed_range_nums[1])
+        end
+        for (seed_index, seed) in enumerate(seeds)
+            for (source_range_index, source_range) in enumerate(source_ranges)
+                if seed in source_range
+                    seeds[seed_index] =
+                        dest_range_starts[source_range_index] + (seed - source_range.start)
+                    break
+                end
+            end
+        end
+    end
+    return minimum(seeds)
+end
 
-# println(part_two("input_part_two.txt"))
+println(part_two("input_part_two.txt"))
--- src/Day 07/day07.jl
+++ src/Day 07/day07.jl
@@ -5,37 +5,15 @@
 # Michael A. Persico (M-PERSIC)
 # https://github.com/M-PERSIC
 
+# TODO
 function part_one(input::String)
-    race_times = parse.(Int, String.(split(split(split(read(input, String), "\n")[1], ":")[2])))
-    race_distances = parse.(Int, String.(split(split(split(read(input, String), "\n")[2], ":")[2])))
-    num_ways = zeros(Int, length(race_times))
-    for (race, race_time) in enumerate(race_times)
-        for i in 0:race_time
-            distance_travelled = i * (race_time - i)
-            if distance_travelled > race_distances[race]
-                num_ways[race] += 1
-            end
-        end
-    end
-    println(num_ways)
-    return reduce(*, num_ways)
+
 end
 
-println(part_one("input_part_one.txt"))
+# println(part_one("input_part_one.txt"))
 
+# TODO
 function part_two(input::String) 
-    lines = String.(split(read(input, String)))
-    dist_index = findfirst(==("Distance:"), lines)
-    race_time = parse(Int, join(lines[2:(dist_index - 1)]))
-    race_distance = parse(Int, join(lines[(dist_index + 1):end]))
-    num_ways = 0
-    for i in 0:race_time
-        distance_travelled = i * (race_time - i)
-        if distance_travelled > race_distance
-            num_ways += 1
-        end
-    end
-    return num_ways
 end
 
-println(part_two("input_part_two.txt"))
+# println(part_two("input_part_two.txt"))
